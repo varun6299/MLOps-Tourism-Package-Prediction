6a1daa494a35de8a984cbd451b6ea12b962999d2
Varun Sagar Theegala <VarunSagar06Official@gmail.com>
2025-11-30 17:15:05 +0530

    Update hosting.py - updating folder path

--- tourism_project/hosting/hosting.py
+++ tourism_project/hosting/hosting.py
@@ -3,7 +3,7 @@ import os
 
 api = HfApi(token=os.getenv("HF_TOKEN"))
 api.upload_folder(
-    folder_path="/content/drive/MyDrive/Masters_2025_AI_ML/MLOps/tourism_project/deployment",     # the local folder containing your files
+    folder_path="tourism_project/deployment",     # the local folder containing your files
     repo_id="Varun6299/Tourism-Package-Prediction",          # the target repo
     repo_type="space",                      # dataset, model, or space
     path_in_repo="",                          # optional: subfolder path inside the repo
